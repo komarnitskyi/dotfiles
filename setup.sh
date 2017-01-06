c8caf387fcfcf7fb44579aea6e3a5f1d7a946920
Andrii Komarnitskyi <ticapac@gmail.com>
2017-01-06 22:24:30 +0200

    add font, fonts installer

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+# copy fonts
+cp ./fonts/* $HOME/Library/Fonts/
+
 # install tilde dotfiles
 python ./sync.py
 
 #install oh-my-fish dotfiles
-python ./sync.py ./omf "$HOME/.config/omf"
+python ./sync.py ./omf $HOME/.config/omf
